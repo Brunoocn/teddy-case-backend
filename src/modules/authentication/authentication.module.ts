e76f2f01960ccf4f41f28c89eabe76f1eacaf9b3
brunoocn <bruno.caneo@contemplato.com>
2025-03-01 15:36:12 -0300

    fix: ajustando dependencias de auth

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -4,10 +4,11 @@ import { LoginService } from './services/login/login.service';
 import { RegisterService } from './services/register/register.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './database/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthenticationController],
-  providers: [LoginService, RegisterService],
+  providers: [LoginService, RegisterService, JwtService],
 })
 export class AuthenticationModule {}
