28fe79575b1920184804152d2bbd341177e33a2f
brunoocn <bruno.caneo@contemplato.com>
2025-03-01 15:33:22 -0300

    feat: configs do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { ConfigModule } from '@nestjs/config';
+import { PluralNamingStrategy } from './config/plural-naming.strategy';
+import { User } from './modules/authentication/database/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/jwt-guard/jwt-auth.guard';
+import { JwtStrategy } from './common/jwt-guard/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthenticationModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DATABASE_HOST,
+      port: parseInt(process.env.PG_DATABASE_PORT),
+      username: process.env.PG_DATABASE_USER,
+      password: process.env.PG_DATABASE_PASSWORD,
+      database: process.env.PG_DATABASE_NAME,
+      entities: [User],
+      synchronize: true,
+      namingStrategy: new PluralNamingStrategy(),
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
