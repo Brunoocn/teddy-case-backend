c72699814b5da3f7c27b53d58fd1826262d5087b
brunoocn <bruno.caneo@contemplato.com>
2025-03-08 18:07:19 -0300

    feat: add swagger

--- src/modules/clients/dtos/find-all-clients-by-user-id.dto.ts
+++ src/modules/clients/dtos/find-all-clients-by-user-id.dto.ts
@@ -5,20 +5,37 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class FindAllClientsByUserIdDTO {
+  @ApiProperty({
+    example: '4154eff6-aefd-4472-bdea-b3bc77d299d7',
+    description: 'ID do usuário associado aos clientes',
+  })
   @IsNotEmpty()
   @IsString()
   readonly userId: string;
 
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Número da página para paginação (opcional)',
+  })
   @IsOptional()
   @IsNumber()
   readonly page: number;
 
+  @ApiPropertyOptional({
+    example: 10,
+    description: 'Quantidade de clientes por página (opcional)',
+  })
   @IsOptional()
   @IsNumber()
   readonly pageSize: number;
 
+  @ApiPropertyOptional({
+    example: true,
+    description: 'Filtrar clientes que possuem "isSelect" ativo (opcional)',
+  })
   @IsOptional()
   @IsBoolean()
   readonly isSelect: boolean;
--- src/modules/clients/dtos/update-is-select-by-client-id.dto.ts
+++ src/modules/clients/dtos/update-is-select-by-client-id.dto.ts
@@ -1,6 +1,11 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateIsSelectByClientIdDTO {
+  @ApiProperty({
+    example: true,
+    description: 'Define se o cliente está selecionado ou não',
+  })
   @IsBoolean()
   readonly isSelect: boolean;
 }
