993ff7bbeeba586d14fcfd01b691ba896b8f9328
brunoocn <bruno.caneo@contemplato.com>
2025-03-08 02:11:00 -0300

    feat: ajustes de arquitetura

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthenticationModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthenticationModule,
+    DatabaseModule,
+    ClientsModule,
+  ],
 })
 export class AppModule {}
